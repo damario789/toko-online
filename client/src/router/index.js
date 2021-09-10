a9a651b58e73075a938bd23077f7a5dc774d1288
damario789 <damario789@gmail.com>
2021-09-10 16:15:37 +0700

    foxshop done

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,29 +1,67 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import History from "../views/History.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
+    beforeEnter: (to, from, next) => {
+      if (!localStorage.access_token) {
+        next("/login");
+      } else {
+        next();
+      }
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/login",
+    name: "Login",
+    component: Login,
+    beforeEnter: (to, from, next) => {
+      if (localStorage.access_token) {
+        next("/");
+      } else {
+        next();
+      }
+    }
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+    beforeEnter: (to, from, next) => {
+      if (localStorage.access_token) {
+        next("/");
+      } else {
+        next();
+      }
+    }
+  },
+  {
+    path: "/history",
+    name: "History",
+    component: History,
+    beforeEnter: (to, from, next) => {
+      if (!localStorage.access_token) {
+        next("/login");
+      } else {
+        next();
+      }
+    }
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
